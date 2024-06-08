c85783d90112fb4df87e9616eee8c81007fd7332
kollence <misterija@gmail.com>
2024-06-08 21:02:59 +0200

    register user method/add role user if not exists

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,7 +13,29 @@ class AuthController extends Controller
 {
     public function register(RegisterUserRequest $request)
     {
+        try{
 
+            $rolesExist = Role::exists();
+            
+            if (!$rolesExist) {
+                $defaultRole = Role::create([
+                    'name' => 'user',
+                ]);
+            }
+            $user = User::create($request->only('name', 'email', 'password'));
+
+            if (isset($defaultRole)) {
+                $user->roles()->attach($defaultRole);
+            }else{
+                $user->roles()->attach([1]);
+            }
+            // // return $user->load('roles');
+            $token = $user->createToken('user-token',['none'])->plainTextToken;
+            $user->token = $token;
+            return new UserResource($user->load('roles'));
+        }catch(\Exception $e){
+            return response()->json(['error' => 'Error: registration failed', 'message' => $e->getMessage()]);
+        }
     }
 
     public function login()
