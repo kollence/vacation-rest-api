bd79b2d1086c0a9425613ed6db5fe1b75f0404fa
kollence <misterija@gmail.com>
2024-06-08 21:01:04 +0200

    Fix RegisterUserRequest pass & add api route

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -24,7 +24,7 @@ class RegisterUserRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed'],
+            'password' => ['required', 'confirmed', 'min:6'],
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1','middleware' => 'auth:sanctum'], function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
