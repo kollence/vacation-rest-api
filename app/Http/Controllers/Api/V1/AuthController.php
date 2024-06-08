2408fae793f545c96b17894c398410aa14760c50
kollence <misterija@gmail.com>
2024-06-08 18:51:28 +0200

    Create AuthController

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+        
+    }
+}
