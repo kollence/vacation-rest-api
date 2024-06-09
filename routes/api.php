c57030e6d8d079366541999da759be072bce7e10
kollence <misterija@gmail.com>
2024-06-09 20:55:25 +0200

    Routes login and for TeamsController

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,24 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\TeamsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1','middleware' => 'auth:sanctum'], function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::get('/teams', [TeamsController::class, 'index']);
+    Route::get('/teams/{id}', [TeamsController::class, 'show']);
+    Route::post('/teams', [TeamsController::class, 'store']);
+    Route::put('/teams/{id}', [TeamsController::class, 'update']);
+    Route::delete('/teams/{id}', [TeamsController::class, 'destroy']);
+    // Auth logout
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
