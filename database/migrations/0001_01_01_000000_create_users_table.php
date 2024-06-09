b7b855901a7a35c5dba9fa00c734aed61857a301
kollence <misterija@gmail.com>
2024-06-09 20:58:12 +0200

    users migration adding team_id column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('team_id')->nullable()->references('id')->on('teams')->onDelete('SET NULL');
             $table->rememberToken();
             $table->timestamps();
         });
