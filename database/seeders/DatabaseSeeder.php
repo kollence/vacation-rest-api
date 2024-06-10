59ca6812b7895307eca5f85aec40bf9e68c6786a
kollence <misterija@gmail.com>
2024-06-10 21:26:23 +0200

    Seeder for users: Loop to add 5 users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,28 +20,15 @@ class DatabaseSeeder extends Seeder
         foreach ($roles as $role) {
             Role::create(['name' => $role]);
         }
-
-        $user = User::factory()->create([
-            'name' => 'User1',
-            'email' => 'user1@mail.com',
-        ]);
-        $roleId = Role::where('name', $roles[0])->first()->id;
-        $user->roles()->attach($roleId);
-        $user->createToken('user-token')->plainTextToken;
-        $user2 = User::factory()->create([
-            'name' => 'User2',
-            'email' => 'user2@mail.com',
-        ]);
-        $roleId = Role::where('name', $roles[0])->first()->id;
-        $user2->roles()->attach($roleId);
-        $user2->createToken('user-token')->plainTextToken;
-        $user3 = User::factory()->create([
-            'name' => 'User3',
-            'email' => 'user3@mail.com',
-        ]);
-        $roleId = Role::where('name', $roles[0])->first()->id;
-        $user3->roles()->attach($roleId);
-        $user3->createToken('user-token')->plainTextToken;
+        foreach(range(1, 5) as $user) {
+            $user = User::factory()->create([
+                'name' => 'User'.$user,
+                'email' => 'user'.$user.'@mail.com',
+            ]);
+            $roleId = Role::where('name', $roles[0])->first()->id;
+            $user->roles()->attach($roleId);
+            $user->createToken('user-token')->plainTextToken;
+        }
 
         $manager = User::factory()->create([
             'name' => 'Manager',
