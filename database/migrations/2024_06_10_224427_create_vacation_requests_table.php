5cfed83864cccb1b2ebea92cedc11bac4f1eab31
kollence <misterija@gmail.com>
2024-06-11 00:52:33 +0200

    VacationRequest model & migration

---
+++ database/migrations/2024_06_10_224427_create_vacation_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vacation_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status',['pending', 'approved', 'rejected'])->default('pending');
+            $table->text('reason')->nullable();
+            $table->foreignId('approved_by')->nullable()->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vacation_requests');
+    }
+};
