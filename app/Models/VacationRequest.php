0c16889a916949b8a74d946209e9938ef40d10be
kollence <misterija@gmail.com>
2024-06-11 00:55:07 +0200

    VacationRequest relationships & scopes

--- app/Models/VacationRequest.php
+++ app/Models/VacationRequest.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class VacationRequest extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'start_date',
+        'end_date',
+        'status',
+        'reason',
+        'approved_by'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function approvedBy()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+
+    public function scopeApproved($query)
+    {
+        return $query->where('status', 'approved');
+    }
+
+    public function scopePending($query)
+    {
+        return $query->where('status', 'pending');
+    }
 }
