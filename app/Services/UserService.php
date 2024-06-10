4106cdfa5bddedbfb8b11e46c84ce5921981743f
kollence <misterija@gmail.com>
2024-06-10 22:53:48 +0200

    Service for creating user & Role logic

---
+++ app/Services/UserService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Role;
+use App\Models\User;
+
+class UserService
+{
+    /**
+     * @param array $data
+     * @return User
+     */
+    public function create(array $data): User
+    {
+        // $defaultRole = config('auth.default_role'); // Use configuration for role name
+        $defaultRole = 'user';
+        try {// Attempt to create the role if it doesn't exist
+            if (!Role::where('name', $defaultRole)->exists()) {
+                Role::create([
+                    'name' => $defaultRole,
+                ]);
+            }
+            $user = new User();
+            $user->name = $data['name'];
+            $user->email = $data['email'];
+            $user->password = $data['password'];
+            $user->save();
+            $user->refresh()->roles()->attach(Role::where('name', $defaultRole)->firstOrFail());
+            return $user;
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
