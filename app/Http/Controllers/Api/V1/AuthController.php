e4673d6a10f83b14934037cf156f3eaf2aba8eb6
kollence <misterija@gmail.com>
2024-06-10 22:54:36 +0200

    Add UserService to register

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -7,44 +7,31 @@ use App\Http\Requests\RegisterUserRequest;
 use App\Http\Resources\ApiUserResource;
 use App\Models\Role;
 use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    public function __construct(
+        protected UserService $userService
+    ) {
+    }
+
+
     public function register(RegisterUserRequest $request)
     {
-        $data = $request->validated(); // Already validated by RegisterUserRequest
 
-        // $defaultRole = config('auth.default_role'); // Use configuration for role name
-        $defaultRole = 'user';
-        try {
-            // Attempt to create the role if it doesn't exist
-            if (!Role::where('name', $defaultRole)->exists()) {
-                Role::create([
-                    'name' => $defaultRole,
-                ]);
-            }
-    
-            $user = User::create([
-                'name' => $data['name'],
-                'email' => $data['email'],
-                'password' => $data['password'],
-            ]);
-            $token = $user->createToken("{$defaultRole}-token")->plainTextToken;
-            
-            $user->token = $token;
-    
-            $user->roles()->attach(Role::where('name', $defaultRole)->firstOrFail());
-    
-            return response()->json([
-                'message' => 'User created successfully',
-                'user' => new ApiUserResource($user),
-            ], 201);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
+        $user = $this->userService->create($request->toArray());
+
+        $token = $user->createToken("user-token")->plainTextToken;
+
+        $user->token = $token;
+
+        return response()->json([
+            'user' => new ApiUserResource($user),
+        ], 201);
     }
 
     public function login(Request $request)
@@ -68,7 +55,7 @@ class AuthController extends Controller
             $user = Auth::user();
 
             $token = $user->createToken("user-token")->plainTextToken;
-                
+
             $user->token = $token;
 
             return response()->json([
