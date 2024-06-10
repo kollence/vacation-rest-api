56d8fe610da61652afa8f924dcd0eed5e74153a7
kollence <misterija@gmail.com>
2024-06-10 23:28:09 +0200

    UserService update()

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -15,7 +15,7 @@ class UserService
     {
         // $defaultRole = config('auth.default_role'); // Use configuration for role name
         $defaultRole = 'user';
-        try {// Attempt to create the role if it doesn't exist
+        try { // Attempt to create the role if it doesn't exist
             if (!Role::where('name', $defaultRole)->exists()) {
                 Role::create([
                     'name' => $defaultRole,
@@ -27,6 +27,19 @@ class UserService
             $user->password = $data['password'];
             $user->save();
             $user->refresh()->roles()->attach(Role::where('name', $defaultRole)->firstOrFail());
+            return $user;
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update(User $user, array $data): User
+    {
+        try {
+            $user->update($data);
+            if (isset($data['role_id']) && count($data['role_id'])) {
+                $user->roles()->sync($data['role_id']);
+            }
             return $user;
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
