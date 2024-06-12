24e33b7d37e94537c261b626833bcc112bcfbaf6
kollence <misterija@gmail.com>
2024-06-12 14:49:09 +0200

    removing manager_id from teams table

--- database/migrations/2024_06_09_154909_create_teams_table.php
+++ database/migrations/2024_06_09_154909_create_teams_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('manager_id')->nullable()->constrained('users')->onDelete('set null');
             $table->timestamps();
         });
     }
